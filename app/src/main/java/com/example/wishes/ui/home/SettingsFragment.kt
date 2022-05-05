11f4d31a25763e10e6ff74fba91f4314fb634ebf
user <svinogr@hotmail.com>
2022-05-05 10:03:29 +0300

    ref main menu

--- app/src/main/java/com/example/wishes/ui/home/SettingsFragment.kt
+++ app/src/main/java/com/example/wishes/ui/home/SettingsFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.wishes.databinding.FragmentHomeBinding
+import com.example.wishes.databinding.FragmentSettingsBinding
 
-class HomeFragment : Fragment() {
+class SettingsFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentSettingsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,9 +23,9 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+            ViewModelProvider(this).get(SettingsViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         val textView: TextView = binding.textHome
--- app/src/main/java/com/example/wishes/ui/home/SettingsViewModel.kt
+++ app/src/main/java/com/example/wishes/ui/home/SettingsViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.wishes.ui.slideshow
+package com.example.wishes.ui.home
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SlideshowViewModel : ViewModel() {
+class SettingsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
 }
